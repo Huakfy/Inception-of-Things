54d716a6f141468a6b9d072bc7b767d51e520157
Huakfy <marcj75009@gmail.com>
2026-05-19 11:27:18 +0200

    Fix p1 script

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -3,7 +3,8 @@
 set -eu
 
 # Install NFS-utils pour le sync-file
-sudo apt-get update & apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y curl nfs-kernel-server
 
 # On attend que le token soit dispo dans le dossier partage
